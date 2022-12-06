a1ab73c8c5e8aeeab9a280b6f4d3c26294a2cd94
root <root@Xeon.localdomain>
2022-12-05 23:40:49 -0600

    created models to get credits/debts

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Ticket;
+
+
 class Customer extends Model
 {
     use HasFactory;
@@ -28,4 +31,34 @@ class Customer extends Model
                             ,'use_company'
                             ,'zip'
                             ,'email'];
+
+
+    public function debts() {
+
+        return $this->hasMany(Ticket::class, 'customer_id', 'id')
+            ->where('refund', false)
+            ->where(function($q) {
+                $q->whereIn('payment_type', ['acct','svc_charge','payment_type','acct_cash', 'acct_check']);
+            })
+           ->selectRaw('customer_id, SUM(total) as sum_total')
+            ->groupBy('customer_id');
+    }
+
+    public function payments() {
+
+        return $this->hasMany(Ticket::class, 'customer_id', 'id')
+            ->whereRaw("payment_type like 'payment_%' or payment_type='discount'")
+        ->selectRaw('customer_id, SUM(total) as sum_total')
+            ->groupBy('customer_id');
+
+    }
+
+    public function returns() {
+
+        return $this->hasMany(Ticket::class, 'customer_id', 'id')
+            ->where('refund', true)
+            ->where('payment_type','acct')
+            ->selectRaw('customer_id, SUM(total) as sum_total')
+            ->groupBy('customer_id');
+    }
 }
