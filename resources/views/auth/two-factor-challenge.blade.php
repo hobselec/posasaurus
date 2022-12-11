30e67220ea71709e938b0b7769b57b7586ed7f41
Jeremy House <jer.house@gmail.com>
2022-12-10 23:11:56 -0600

    changed login views back to Breeze

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,14 +1,33 @@
-@extends('layouts.app')
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-@section('content')
-<form method="POST" action="/pos/two-factor-challenge">
-    @csrf
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <label>{{ __('Code') }}</label>
-    <input type="text" name="code" />
+        <form method="POST" action="/pos/two-factor-challenge">
+            @csrf
 
-    <button>
-        Login
-    </button>
-</form>
-@endsection
+            <!-- TOTP code -->
+            <div>
+                <x-input-label for="code" value="2FA Code" />
+                <x-text-input id="code" class="block mt-1 w-full" type="text" pattern="[0-9]*" inputmode="numeric" name="code" autocomplete="off" required autofocus />
+                
+                @if(str_contains(Request::server('HTTP_REFERER'), 'two-factor-challenge'))
+                <x-input-error :messages="['error'=>'The code is invalid']" class="mt-2" />
+                @endif
+          
+            </div>
+
+
+                <x-primary-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
