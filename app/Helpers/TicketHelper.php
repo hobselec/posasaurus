98d10bc4cd8c7c4f1a8f37c1d93e1930d06172fd
Jeremy House <jer.house@gmail.com>
2022-12-13 23:02:15 -0600

    calculation not accounting for tax exempt

--- app/Helpers/TicketHelper.php
+++ app/Helpers/TicketHelper.php
@@ -12,8 +12,18 @@ class TicketHelper {
     {
         
         $ticket->subtotal = $ticket->items->sum('amount') - $ticket->discount;
-        $ticket->tax = round($ticket->subtotal * Config::get('pos.sales_tax'), 2);
 
+        $taxExempt = false;
+        if($ticket->customer)
+        {
+            if($ticket->customer->tax_exempt)
+                $taxExempt = true;
+        }
+
+        if(!($ticket->resale || $taxExempt))
+            $ticket->tax = round($ticket->subtotal * Config::get('pos.sales_tax'), 2);
+        else
+            $ticket->tax = 0;
         
         $ticket->total = round($ticket->subtotal + $ticket->tax,2);
       // $ticket->subtotal = number_format($ticket->subtotal, 2);
