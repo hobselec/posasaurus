ed15800b5073ed17f64324983eb945a2fb13ba9a
Jeremy House <jer.house@gmail.com>
2023-01-01 21:35:10 -0600

    add fillable, accessors

--- app/Models/CatalogItem.php
+++ app/Models/CatalogItem.php
@@ -11,5 +11,24 @@ class CatalogItem extends Model
 
     protected $table = 'catalog';
 
-    
+    protected $fillable = ['barcode','name','manufacturer_id','price','qty','sku','vendor'];
+
+    public function getVendorNameAttribute($value)
+    {
+        return $value ?? '';
+    }
+    public function getManufacturerIdAttribute($value)
+    {
+        return $value ?? '';
+    }
+
+    public function getProductIdAttribute($value)
+    {
+        return $value ?? '';
+    }
+
+    public function getBarcodeAttribute($value)
+    {
+        return $value ?? '';
+    }
 }
