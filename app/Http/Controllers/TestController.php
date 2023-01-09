89359ed29616227d51c12df422178c78fef7be5f
Jeremy House <jer.house@gmail.com>
2023-01-08 22:08:44 -0600

    ReceiptEmail accepts a string now

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Mail\ReceiptEmail;
 use Illuminate\Support\Facades\Mail;
-
+use Config;
 
 class TestController extends Controller
 {
     public function mail()
     {
-        $obj = (object) ['receipt' => 'stuff'];
+        //$obj = (object) ['receipt' => 'stuff'];
+        $str = 'this is the email';
 
-        Mail::to('jer.house@gmail.com')->send(new ReceiptEmail($obj));
+        Mail::to('jer.house@gmail.com')->send(new ReceiptEmail($str));
 
         echo 'ok';
     }
