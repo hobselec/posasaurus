10c90c511086954f3074bba347fffa9854ff11db
Jeremy House <jer.house@gmail.com>
2023-01-10 21:52:56 -0600

    add nullable to email

--- database/migrations/2022_12_05_041954_change_booleans_on_customers.php
+++ database/migrations/2022_12_05_041954_change_booleans_on_customers.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->boolean('use_company')->default(false)->change();
             $table->boolean('credit')->default(true)->change();
             $table->boolean('print_statement')->default(false)->change();
-            $table->char('email', 100);
+            $table->char('email', 100)->nullable();
         
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
