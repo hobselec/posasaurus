a1182fd81d2766586971ed80dec625f95f6d5224
Jeremy House <jer.house@gmail.com>
2023-01-17 20:56:20 -0600

    show current users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="content" style="font-family: sans-serif">
+
 <h2>
     You are signed in as {{ auth()->user()->name }}
 </h2>
 
+
+<h2>
+    Users
+</h2>
+
+@foreach($users as $user)
+<table style="min-width: 500px; border: 1px solid #000000; border-collapse: collapse">
+    <tr>
+    <th>Name</th><th>Username</th><th>Last Login</th>
+    </tr>
+    <tr>
+    <td>{{ $user->name }}</td>
+    <td>{{ $user->email }}</td>
+    <td>{{ $user->updated_at }}</td>
+</tr>
+</table>
+
+@endforeach
+
+
+<two-factor-auth :enabled="{{ json_encode(auth()->user()->twoFactorAuthEnabled()) }}" />
+
+<!--
 <form method="POST" action="{{ route('logout') }}">
     @csrf
     <button>Logout</button>
 </form>
+-->
 
-<two-factor-auth :enabled="{{ json_encode(auth()->user()->twoFactorAuthEnabled()) }}" />
-    
+</div>
 @endsection
