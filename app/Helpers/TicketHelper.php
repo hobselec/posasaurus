944516831e387c7a8fe21788c501bd379f06fcc7
Jeremy House <jer.house@gmail.com>
2023-01-23 23:12:55 -0600

    fix issues with subtotal and tax

--- app/Helpers/TicketHelper.php
+++ app/Helpers/TicketHelper.php
@@ -11,7 +11,7 @@ class TicketHelper {
     public static function computeTotals(Ticket &$ticket)
     {
         
-        $ticket->subtotal = $ticket->items->sum('amount') - $ticket->discount + $ticket->labor + $ticket->freight;
+        $ticket->subtotal = $ticket->items->sum('amount');
 
         $taxExempt = false;
         if($ticket->customer)
@@ -20,12 +20,14 @@ class TicketHelper {
                 $taxExempt = true;
         }
 
+        $adjustment = $ticket->discount + $ticket->labor + $ticket->freight;
+
         if(!($ticket->resale || $taxExempt))
-            $ticket->tax = round($ticket->subtotal * Config::get('pos.sales_tax'), 2);
+            $ticket->tax = round(($ticket->subtotal - $adjustment) * Config::get('pos.sales_tax'), 2);
         else
             $ticket->tax = 0;
         
-        $ticket->total = round($ticket->subtotal + $ticket->tax,2);
+        $ticket->total = round($ticket->subtotal - $adjustment + $ticket->tax, 2);
       // $ticket->subtotal = number_format($ticket->subtotal, 2);
        // $ticket->tax = number_format($ticket->tax, 2);
 
