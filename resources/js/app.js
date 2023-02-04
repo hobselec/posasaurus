6b1fb562e634c62de77f5bc086f9a3207965627a
Jeremy House <jer.house@gmail.com>
2023-02-03 23:45:35 -0600

    reconfigured to use Ably

--- resources/js/app.js
+++ resources/js/app.js
@@ -51,13 +51,12 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-     broadcaster: 'pusher',
-     key: import.meta.env.VITE_PUSHER_APP_KEY,
-     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-     enabledTransports: ['ws', 'wss'],
-     cluster:import.meta.env.VITE_PUSHER_APP_CLUSTER,
-     authEndpoint: '/pos/broadcasting/auth'
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_ABLY_PUBLIC_KEY,
+    wsHost: 'realtime-pusher.ably.io',
+    wsPort: 443,
+    disableStats: true,
+    encrypted: true,
+    cluster:import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    authEndpoint: '/pos/broadcasting/auth'
 });
