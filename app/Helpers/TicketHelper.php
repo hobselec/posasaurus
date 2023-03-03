6cc7b80d3dc8ea11e27486219e90c41a6b53e852
Jeremy House <jer.house@gmail.com>
2023-03-03 15:19:29 -0600

    discount,tax, freight, labor adjustments calculation

--- app/Helpers/TicketHelper.php
+++ app/Helpers/TicketHelper.php
@@ -20,14 +20,14 @@ class TicketHelper {
                 $taxExempt = true;
         }
 
-        $adjustment = $ticket->discount + $ticket->labor + $ticket->freight;
+        $adjustment = -1 * $ticket->discount + $ticket->labor + $ticket->freight;
 
         if(!($ticket->resale || $taxExempt))
-            $ticket->tax = round(($ticket->subtotal - $adjustment) * Config::get('pos.sales_tax'), 2);
+            $ticket->tax = round(($ticket->subtotal - $ticket->discount) * Config::get('pos.sales_tax'), 2);
         else
             $ticket->tax = 0;
         
-        $ticket->total = round($ticket->subtotal - $adjustment + $ticket->tax, 2);
+        $ticket->total = round($ticket->subtotal + $adjustment + $ticket->tax, 2);
       // $ticket->subtotal = number_format($ticket->subtotal, 2);
        // $ticket->tax = number_format($ticket->tax, 2);
 
